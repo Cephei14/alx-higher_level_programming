febc7ef40e3f6ccf6683c96a9ca9259f709131a4
Cephei14 <rafik.dhaibi@gmail.com>
2023-11-28 18:46:53 -0800

    5&4; p

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 99, 1):
-    print(f"{i:d} = {hex(i)}")
+    print("{:d} = {}".format(i, hex(i)))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 for i in range(0, 10, 1):
     for j in range(0, 10, 1):
         if i == 9 and j == 9:
-            print(f"{i}{j}", end="\n")
+            print("{:d}{:d}".format(i, j))
             break
         else:
-            print(f"{i}{j}", end=", ")
+            print("{:d}{:d}".format(i, j), end=", ")
