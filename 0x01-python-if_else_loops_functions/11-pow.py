492aad5de672a73e5efe81e3a396c789d816376f
Cephei14 <rafik.dhaibi@gmail.com>
2023-11-30 01:41:31 -0800

    11 12

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def pow(a, b):
+    c = 1
+    if b > 0:
+        if a >= 0:
+            for i in range(b):
+                c = c*a
+            return c
+        if a < 0:
+            for i in range(b):
+                c = c * a
+            return c
+    elif b == 0:
+        return 1
+    else:
+        for i in range(-b):
+            c = c / a
+        return c
