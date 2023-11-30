8fcea0b7ca422f7a7e6ba3154276188883ae05c6
Cephei14 <rafik.dhaibi@gmail.com>
2023-11-29 19:35:19 -0800

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 9, 1)
+for i in range(0, 9, 1):
+    for j in range(i+1, 10, 1):
+        if (i == 8):
+            print("{:d}{:d}".format(i, j), end="\n")
+            break
+        if i != j:
+            print("{:d}{:d}".format(i, j), end=", ")
