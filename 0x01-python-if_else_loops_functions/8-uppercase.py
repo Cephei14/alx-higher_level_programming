d269e7bb7da09d4e9328c7647e3619b3aefa7ae5
Cephei14 <rafik.dhaibi@gmail.com>
2023-11-29 21:39:06 -0800

    tasks 9 10

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,9 @@
-#!/usr/bin/python3
-
+def uppercase(str):
+    Upstr = ""
+    for c in str:
+        if 'a' <= c <= 'z':
+            Nc = chr(ord(c) - 32)
+            Upstr += Nc
+        else:
+            Upstr += c
+    print(Upstr)
