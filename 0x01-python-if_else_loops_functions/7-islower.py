aefe78775b5e66e8bff74e53d309f64ed4d2587d
Cephei14 <rafik.dhaibi@gmail.com>
2023-11-29 21:40:43 -0800

    tasks 7 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def islower(c):
     if c < chr(97) or c > chr(122):
         return False
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def uppercase(str):
     Upstr = ""
     for c in str:
