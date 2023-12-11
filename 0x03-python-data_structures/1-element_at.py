c905ceea448ca53e88b285daed5e5a92424eba73
Cephei14 <rafik.dhaibi@gmail.com>
2023-12-11 00:47:13 -0800

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     lilen = len(my_list)
-    if lilen <= idx < 0:
-        return None
+    if lilen < idx < 0:
+        return "None"
     else:
         return my_list[idx]
