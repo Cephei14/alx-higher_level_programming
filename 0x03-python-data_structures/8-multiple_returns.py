79685210b77273a996509d440fe29c5287ff7d1c
Cephei14 <rafik.dhaibi@gmail.com>
2023-12-11 19:03:51 -0800

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
+    if sentence == "":
         return (0, None)
     else:
         length = len(sentence)
         char = sentence[0]
         return (length, char)
-
