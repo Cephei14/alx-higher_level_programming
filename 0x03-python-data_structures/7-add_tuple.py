e9cf6bc6376eacc575674b91ed769397def21f9c
Cephei14 <rafik.dhaibi@gmail.com>
2024-03-17 19:37:03 -0700

    ex07

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,14 +2,14 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 0:
         if len(tuple_b) == 0:
-            return (0,0)
+            return (0, 0)
         elif len(tuple_b) == 1:
             return (tuple_b[0], 0)
         else:
             return (tuple_b[0], tuple_b[1])
     elif len(tuple_b) == 0:
         if len(tuple_a) == 0:
-            return (0,0)
+            return (0, 0)
         elif len(tuple_a) == 1:
             return (tuple_a[0], 0)
         else:
@@ -29,4 +29,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             x = tuple_a[0] + tuple_b[0]
             y = tuple_a[1] + tuple_b[1]
-    return (x,y)
+    return (x, y)
