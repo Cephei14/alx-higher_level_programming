539a8c030adacd509791c40f56512f4c13a64003
Cephei14 <rafik.dhaibi@gmail.com>
2024-03-18 02:56:47 -0700

    ex9

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     n_list = []
-    for number in my_list:
-        if (number % 2) == 0:
+    for i in my_list:
+        if (i % 2) == 0:
             n_list.append(True)
         else:
             n_list.append(False)
