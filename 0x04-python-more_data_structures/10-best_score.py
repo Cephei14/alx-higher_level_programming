5ffdd2f6eec5516578b8453034465c67d3aa0c96
Cephei14 <rafik.dhaibi@gmail.com>
2024-03-20 19:56:37 -0700

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-
+    if a_dictionary is not None:
+        number = len(a_dictionary.values())
+        if number > 0:
+            result = max(a_dictionary.values())
+            for i in a_dictionary.keys():
+                if a_dictionary[i] == result:
+                    return i
+    return None
