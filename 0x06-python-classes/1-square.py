714cb6dfa8941a31e849db403176171d4d3878c1
Cephei14 <rafik.dhaibi@gmail.com>
2024-05-21 22:24:55 +0900

    task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Here we upgrade the square class a little"""
+
+
+class Square:
+    """added a size of the square"""
+    def __init__(self, size):
+        self.__size == size
+
